c355a6f87291c1cb1369868a42c4fdde81667d4b
Tallal Mohar <tallalmoohar@gmail.com>
2025-04-07 12:10:35 -0700

    First Stage

---
+++ taskTracker.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class taskTracker{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); //scanner stuff
+		
+		System.out.println("Welcome to TaskTracker CLI Tool!");
+		System.out.println("Enter (C) to see all commands!");
+		
+		
+	}
+}
